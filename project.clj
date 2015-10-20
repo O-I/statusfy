892e33f84fb7df8fb18ed70134d6b11849892268
Rahul Horé <raevyn_@hotmail.com>
2015-10-19 23:45:20 -0400

    Use wrap-reload middleware
    Allow code changes without manually restarting the server.
    Only times a manual restart will be needed is if we want to add a new library or
    if we want to change our main function again.
    
    1. Include ring middleware reload namespace (we only want `wrap-reload`)
    2. Add a `-dev-main` function that will only be called in development
    3. Create a dev profile in `project.clj` to ensure `-dev-main` runs during development

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring "1.4.0"]]
-  :main statusfy.core)
+  :main statusfy.core
+  :profiles {:dev
+             {:main statusfy.core/-dev-main}})
