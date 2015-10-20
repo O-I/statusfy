a8f2b73c3b0e48d5a86e5e6741dddb17e9108540
Rahul Horé <raevyn_@hotmail.com>
2015-10-19 23:40:52 -0400

    Respond with 200 for root path only; 404 otherwise

--- src/statusfy/core.clj
+++ src/statusfy/core.clj
@@ -1,10 +1,14 @@
 (ns statusfy.core
   (:require [ring.adapter.jetty :as jetty]))
 
-(defn status [req]
-  {:status 200
-   :body "Statusfy is up!"
-   :headers {}})
+(defn status [{:keys [uri] :as req}]
+  (if (= uri "/")
+    {:status 200
+     :body "Statusfy is up!"
+     :headers {}}
+    {:status 404
+     :body "Can't get no Statusfaction"
+     :headers {}}))
 
 (defn -main [port]
   (jetty/run-jetty status
