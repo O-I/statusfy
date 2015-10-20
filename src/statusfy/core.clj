0e97139d770619cb2472dab1e83a40c6ea416858
Rahul Horé <raevyn_@hotmail.com>
2015-10-19 23:34:33 -0400

    Refactor handler to use status function

--- src/statusfy/core.clj
+++ src/statusfy/core.clj
@@ -1,9 +1,11 @@
 (ns statusfy.core
   (:require [ring.adapter.jetty :as jetty]))
 
+(defn status [req]
+  {:status 200
+   :body "Statusfy is up!"
+   :headers {}})
+
 (defn -main [port]
-  (jetty/run-jetty (fn [req]
-                     {:status 200
-                      :body "Statusfy!"
-                      :headers {}})
+  (jetty/run-jetty status
                    {:port (Integer. port)}))
